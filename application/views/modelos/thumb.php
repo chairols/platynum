c9abe3e2265412c49219020ee3c17b5c95a36956
chairols <hernanbalboa@gmail.com>
2018-10-19 00:28:20 -0300

    - bug fixes

--- application/views/modelos/thumb.php
+++ application/views/modelos/thumb.php
@@ -2,11 +2,13 @@
     <section class="content">
         <img id="image" src="/Fotodisk/<?= $modelo['perfil'] ?>/<?= $modelo['carpeta'] ?>/<?= $modelo['carpeta'] ?><?= $idfoto ?>.jpg">
 
-        <button onclick="crop()" class="btn btn-primary">
-            Boton
-        </button>
+        <div class="text-center">
+            <button onclick="crop()" class="btn btn-primary">
+                Crear Thumb
+            </button>
+        </div>
     </section>
 </div>
 
-<input id="idfoto" value="<?=$idfoto?>" type="hidden">
-<input id="idmodelo" value="<?=$modelo['ID']?>" type="hidden">
+<input id="idfoto" value="<?= $idfoto ?>" type="hidden">
+<input id="idmodelo" value="<?= $modelo['ID'] ?>" type="hidden">
